e7e79ae98ae538d496361abd2e5728e03d03af75
skaushal001 <skaushalsingh281@gmail.com>
2024-12-20 09:46:00 +0530

    upload code

---
+++ ques1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    // Input: Read the value of n
+    cin >> n;
+
+    // Calculate the sum using the formula
+    int sum = n * (n + 1) / 2;
+
+    // Output the result
+    cout << sum << endl;
+
+    return 0;
+}
